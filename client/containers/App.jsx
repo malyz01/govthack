7c7df8fb2a7104e552b2fef948699e5e6f2af2b0
malyz <mattuy001@gmail.com>
2020-08-08 14:49:25 +1200

    UPDATE: App.jsx - changed to browserrouter, ADDED: App.jsx - page1 route

--- client/containers/App.jsx
+++ client/containers/App.jsx
@@ -1,13 +1,17 @@
 import React from 'react'
-import { HashRouter, Route } from 'react-router-dom'
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
 
 import Landing from './_Landing'
+import Page1 from './_Page1'
 
 const App = () => {
   return (
-    <HashRouter>
-      <Route exact path="/" component={Landing} />
-    </HashRouter>
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/" component={Landing} />
+        <Route exact path="/page1" component={Page1} />
+      </Switch>
+    </BrowserRouter>
   )
 }
 
