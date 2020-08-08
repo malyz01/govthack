33e42420df2b7254a74f27809626cbdca1c238f7
malyz <mattuy001@gmail.com>
2020-08-09 09:41:56 +1200

    UDPATE: server.js - added server.use for user route

--- server/server.js
+++ server/server.js
@@ -13,6 +13,8 @@ server.use(express.urlencoded({ extended: false }))
 server.use(express.json())
 server.use(express.static(path.resolve('server', 'public')))
 
+server.use('/api/v1/user', require('./routes/user'))
+
 server.get('*', (_, res) => {
   res.sendFile(path.resolve('server', 'public', 'index.html'))
 })
