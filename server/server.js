0aca00d8fed6366eef2d8b173d86ebaa2d60c642
malyz <mattuy001@gmail.com>
2020-08-08 14:53:11 +1200

    UPDATE: server.js - added validation if in production, added get * route

--- server/server.js
+++ server/server.js
@@ -1,4 +1,5 @@
-require('dotenv').config()
+const IN_PROD = process.env.NODE_ENV === 'production'
+!IN_PROD && require('dotenv').config()
 const path = require('path')
 const express = require('express')
 const cors = require('cors')
@@ -9,5 +10,9 @@ server.use(cors())
 server.use(express.urlencoded({ extended: false }))
 server.use(express.json())
 server.use(express.static(path.resolve('server', 'public')))
+
+server.get('*', (_, res) => {
+  res.sendFile(path.resolve('server', 'public', 'index.html'))
+})
 
 module.exports = server
