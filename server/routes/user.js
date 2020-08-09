065543b0d7fb7ba114545d0f00caadb252fb5e99
malyz <mattuy001@gmail.com>
2020-08-09 15:13:07 +1200

    UPDATE: routes/user.js - added module.exports

--- server/routes/user.js
+++ server/routes/user.js
@@ -3,4 +3,6 @@ const router = require('express').Router()
 const User = require('../db/controllers/user')
 
 // POST - api/v1/user
-router.post('/user', User.registerUser)
+router.post('/', User.registerUser)
+
+module.exports = router
