a86e7255069cb8aab2073eb0d0c510b4a04ad0cf
malyz <mattuy001@gmail.com>
2020-08-15 07:55:07 +1200

    added layout and styles

--- client/containers/_Pages/Landing/index.jsx
+++ client/containers/_Pages/Landing/index.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import './style.css'
 
 import Map from './Map'
 import cities from '../../json/cities.json'
@@ -7,17 +8,23 @@ const geo = [-40.839618, 174.175857]
 
 const Landing = () => {
   return (
-    <div>
-      <Map geo={geo} data={cities}>
-        {(data) => (
-          <img
-            className="mapMarkIcon"
-            width="20px"
-            src="/assets/images/mark.svg"
-            onClick={() => alert(data.name)}
-          />
-        )}
-      </Map>
+    <div className="landingContainer">
+      <section className="contentContainer">
+        <div>Data to be displayed</div>
+      </section>
+
+      <section className="mapContainer">
+        <Map geo={geo} data={cities}>
+          {(data) => (
+            <img
+              className="mapMarkIcon"
+              width="20px"
+              src="/assets/images/mark.svg"
+              onClick={() => alert(data.name)}
+            />
+          )}
+        </Map>
+      </section>
     </div>
   )
 }
