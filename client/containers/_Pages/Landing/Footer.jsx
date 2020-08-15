9597a20bbc9c1bbd4612918a80ffa9b6102378d2
arielwc <arielwalkercochrane@gmail.com>
2020-08-15 18:51:17 +1200

    adds social links

--- client/containers/_Pages/Landing/Footer.jsx
+++ client/containers/_Pages/Landing/Footer.jsx
@@ -4,6 +4,10 @@ function Footer(){
     return(
         <footer className="footer">
             copyright
+            <div className="socials">
+            <img aria-label= 'Youtube link' src='../../server/public/assets/images/youtube.svg' />
+            <img aria-label= 'Github link' src='../../server/public/assets/images/github.svg' />
+            </div>
         </footer>
     )
 }
