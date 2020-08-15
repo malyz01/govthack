ff05a7bf3df8b3a94031e3568565b72ad1129098
Chris <chriskirbydev@gmail.com>
2020-08-15 18:49:13 +1200

    Update footer, import google fonts, update styling

--- client/containers/_Pages/Landing/Footer.jsx
+++ client/containers/_Pages/Landing/Footer.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 
 function Footer(){
+    const currentYear = new Date().getFullYear();
     return(
         <footer className="footer">
-            copyright
+            <p className="copyright">Copyright ⓒ {currentYear}</p>
         </footer>
     )
 }
