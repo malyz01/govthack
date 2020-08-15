e5075e5ee5a2009d58260aa7da38ed20acf234c3
malyz <mattuy001@gmail.com>
2020-08-16 09:03:09 +1200

    removed unused codes

--- server/db/controllers/user.js
+++ server/db/controllers/user.js
@@ -1,32 +1,11 @@
 const User = require('../models/User')
 
-async function registerUser(req, res, next) {
-  try {
-    const userData = req.body
-    const user = await User.create(userData)
-    const { id, email } = user
-    res.status(200).json({ id, email })
-  } catch (e) {
-    console.error(e)
-    const errors = e.errors ? Object.keys(e.errors) : []
-    // create your own custom error
-    if (errors.length) {
-      const messages = errors.map((err) => e.errors[err].message)
-      return res.status(400).json(messages)
-    }
-    if (e.code && e.code === 11000)
-      return res.status(400).json('Email already taken')
-    res.status(400).json('Something went wrong, please try again later...')
-  }
+function registerUser(req, res, next) {
+  res.json('Write logic for register User')
 }
 
 function loginUser(req, res, next) {
-  try {
-    const userData = req.body
-    // Add logic for login
-  } catch (e) {
-    console.error(e)
-  }
+  res.json('Write logic for login User')
 }
 
 module.exports = {
